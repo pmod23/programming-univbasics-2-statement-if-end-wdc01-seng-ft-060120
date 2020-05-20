7af41d90b39ed4fbfd182332585bd49649df4a62
Pramod Thomas <pmod23@gmail.com>
2020-05-20 13:11:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,4 +4,14 @@ if run_code_inside
   puts "code inside"
 end
 puts "Code after if...end"
-# Write your solution here
+current_time =
+Time.now 
+current_time =
+current_time.to_i 
+
+if current_time.even? 
+  puts "Even!" 
+  else 
+    puts "Odd!" 
+    end 
+    # Write your solution here
