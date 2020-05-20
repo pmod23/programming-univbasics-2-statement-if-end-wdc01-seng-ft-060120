9dc1f2c86fc63625119d9c964509cc2cb484f3a2
Pramod Thomas <pmod23@gmail.com>
2020-05-20 13:12:33 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,17 @@
-# Write your solution here
+run_code_inside = false 
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+current_time =
+Time.now 
+current_time =
+current_time.to_i 
+
+if current_time.even? 
+  puts "Even!" 
+  else 
+    puts "Odd!" 
+    end 
+    # Write your solution here
